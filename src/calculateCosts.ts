9252a68e4cb1344c8d3c0384713975e36af94752
Hannah <hannah144@protonmail.com>
2022-09-23 12:15:39 +0100

    publishing

--- src/calculateCosts.ts
+++ src/calculateCosts.ts
@@ -157,7 +157,8 @@ export async function calculatePerOrderFeePerMonth<T>(data: T & TOrderFeeCalcula
 {
   console.log("calculatePerOrderFeePerMonth",
     data.basketLimit, data.cost, data.listingsPerMonth, data.deliveryPrice, data.baseTax, data.quantity,
-    data.nBundleProducts)
+    data.nBundleProducts);
+
   const maxListingsPerOrder = Math.floor(data.basketLimit / data.cost) || 1;
   const ordersPerMonth = data.listingsPerMonth / maxListingsPerOrder;
 
