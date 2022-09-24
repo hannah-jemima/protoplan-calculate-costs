ea698460bc76e951cc7f6baff090fd77d5633a12
Hannah <hannah144@protonmail.com>
2022-09-24 19:04:41 +0100

    publishing

--- src/enforceTypes.ts
+++ src/enforceTypes.ts
@@ -3,16 +3,16 @@
 
 export function enforceProtocolRowTypes<T>(r: T & {
   price: number,
-  baseTax: number,
-  taxPercent: number,
-  deliveryPrice: number,
-  amount: number })
+  baseTax?: number,
+  taxPercent?: number,
+  deliveryPrice?: number,
+  amount?: number })
 {
   return {
     ...r,
     price: Number(r.price),
-    baseTax: Number(r.baseTax),
-    taxPercent: Number(r.taxPercent),
-    deliveryPrice: Number(r.deliveryPrice),
-    amount: Number(r.amount) };
+    baseTax: r.baseTax !== undefined ? Number(r.baseTax) : undefined,
+    taxPercent: r.taxPercent !== undefined ? Number(r.taxPercent) : undefined,
+    deliveryPrice: r.deliveryPrice !== undefined ? Number(r.deliveryPrice) : undefined,
+    amount: r.amount !== undefined ? Number(r.amount) : undefined };
 }
