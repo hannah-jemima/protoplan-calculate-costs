2f6e2d65a9f54137d376313d2c5894662b7c3551
Hannah <hannah144@protonmail.com>
2022-10-22 18:19:19 +0100

    publishing

--- src/enforceTypes.ts
+++ src/enforceTypes.ts
@@ -1,20 +1,34 @@
 
-
-
-export function enforceProtocolRowTypes<T>(r: T & {
+interface IEnforcableProps
+{
   price: number,
   baseTax?: number,
   taxPercent?: number,
   deliveryPrice?: number,
-  amount?: number,
-  scrapeTime?: Date | null })
+  amount: number,
+  amountUnit: string,
+  scrapeTime?: Date | null,
+  basketLimit?: number | null
+}
+
+export function enforceProtocolRowTypes<T>(rows: (T & IEnforcableProps)[])
 {
-  return {
+  return rows.map(r => ({
     ...r,
     price: Number(r.price),
     baseTax: r.baseTax !== undefined ? Number(r.baseTax) : undefined,
     taxPercent: r.taxPercent !== undefined ? Number(r.taxPercent) : undefined,
     deliveryPrice: r.deliveryPrice !== undefined ? Number(r.deliveryPrice) : undefined,
-    amount: r.amount !== undefined ? Number(r.amount) : undefined,
-    scrapeTime: r.scrapeTime ? new Date(r.scrapeTime) : r.scrapeTime };
+    amount: Number(r.amount),
+    amountUnit: String(r.amountUnit),
+    scrapeTime: r.scrapeTime ? new Date(r.scrapeTime) : r.scrapeTime,
+    basketLimit: r.basketLimit ? Number(r.basketLimit) : r.basketLimit }));
+}
+
+
+export function enforceListingTableTypes<T>(listings: (T & IEnforcableProps & { priceWithTax: number })[])
+{
+  return enforceProtocolRowTypes(listings).map(l => ({
+    ...l,
+    priceWithTax: Number(l.priceWithTax) }));
 }
